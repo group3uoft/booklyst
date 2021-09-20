ae4ace67795131cc2d4ad65270dfd5bbd8a2d37c
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-09-20 17:14:30 -0400

    fix the Browser page

--- client/src/components/Swiper/index.js
+++ client/src/components/Swiper/index.js
@@ -2,6 +2,7 @@
 
 import React, {useState, useEffect} from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
+import css from './index.css'
 import BookCard from "../BookCard"
 // import { useSelector, useDispatch } from "react-redux";
 // import SwiperCore from 'swiper';
@@ -10,9 +11,13 @@ import BookCard from "../BookCard"
 // Import Swiper styles
  import 'swiper/swiper.scss';
  import 'swiper/components/navigation/navigation.scss'
+ import './index.css'
 //  import 'swiper/components/navigation/pagination.scss'
 //  import 'swiper/components/navigation/scrollbar.scss'
+import SwiperCore, { Pagination, Navigation } from 'swiper';
 
+// install Swiper modules
+SwiperCore.use([Pagination, Navigation]);
 
 
 export default ({searchedBooks}) => {
@@ -21,22 +26,24 @@ export default ({searchedBooks}) => {
 
   return (
     <Swiper
-       spaceBetween={50}
-      slidesPerView={4}
-      navigation
-    //   pagination={{clickable:true}}
-     // scrollbar={{draggble:true}}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-      // style={{padding:'5rem'}}
+      slidesPerView={3}
+      spaceBetween={30}
+      slidesPerGroup={3}
+      loop={true}
+      loopFillGroupWithBlank={true}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      className="mySwiper"
     >
-    {searchedBooks && searchedBooks.map((book) => (
-          <SwiperSlide key={book.bookId}><BookCard book={book} /></SwiperSlide>
+      {searchedBooks &&
+        searchedBooks.map((book) => (
+          <SwiperSlide key={book.bookId}>
+            <BookCard book={book} />
+          </SwiperSlide>
         ))}
-     
-      
       ...
     </Swiper>
   );
 };
-
