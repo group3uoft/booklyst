ea4e633d91fc4a80f0fbe0f2bdc432b434592813
Clinton <80900245+pravton@users.noreply.github.com>
2021-09-22 23:29:21 -0400

    updated voice function

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,21 +1,40 @@
 import React, { useEffect, useState } from "react";
 import bgImg from '../../assets/images/hero-bg.jpg'
 import { deepSearchHandle } from "../../utils/helpers";
-import ImageUpload from "../UploadImage";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 export default function Hero({setSearchedBooks, setSearchInput, searchInput, setSearchHistory, searchedBooks}) {
+  const {
+    transcript,
+    listening,
+    // resetTranscript,
+    // browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
 
-  const [imageLoading, setImageLoading] = useState("");
-import SpeechRecognition, {
-  useSpeechRecognition,
-} from "react-speech-recognition";
+  const [voiceSearch, setVoiceSearch] = useState("");
 
-export default function Hero({
-  setSearchedBooks,
-  setSearchInput,
-  searchInput,
-  setSearchHistory,
-}) {
+  // function onChangeHandler() {
+  //   setVoiceSearch("");
+  // }
+
+  useEffect(() => {
+    setVoiceSearch(transcript);
+  }, [listening])
+
+  useEffect(() => {
+      if(voiceSearch) {
+        async function fetchData() {
+          const data = await deepSearchHandle(voiceSearch);
+          await setSearchedBooks(data);
+          setSearchHistory(data);
+        }
+  
+        setVoiceSearch("");
+  
+        fetchData();
+      }
+  }, [voiceSearch]);
+
   const searchSubmit = async (e) => {
     e.preventDefault();
     const query = e.target[0].value;
@@ -25,30 +44,7 @@ export default function Hero({
     await setSearchedBooks(data);
     setSearchHistory(query);
   }
-    setSearchHistory(searchInput);
-  };
-  const [message, setMessage] = useState("");
-  const commands = [
-    {
-      command: "*",
-      callback: (text) => {
-        // from here you can grab the text and search for the text
-        console.log(text);
-        setMessage(text);
-      },
-    },
-  ];
-  const {
-    initialTranscript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition,
-  } = useSpeechRecognition({ commands });
 
-  const onReset = () => {
-    setMessage("");
-    resetTranscript();
-  };
   useEffect(() => {
     async function fetchData() {
       const data = await deepSearchHandle(searchInput);
@@ -59,63 +55,37 @@ export default function Hero({
     }
   }, [searchInput, setSearchInput, setSearchedBooks, searchedBooks]);
 
-  // return(
-  //   <div className="d-flex justify-content-center align-items-center hero-bg" style={{backgroundImage: `url(${bgImg})`}}>
-  //     <div className="search-container d-flex justify-content-center flex-column p-4">
-  //       <form className="d-flex" onSubmit={searchSubmit}>
-  //         <div className="input-container d-flex">
-  //           <input 
-  //           className="form-control mr-sm-2" 
-  //           name="search"
-  //           type="search" 
-  //           placeholder="Search books, ISBN, Author" 
-  //           aria-label="Search" 
-  //           // value={imageLoading}
-  //           // onChange={(e) => setSearchInput(e.target.value)}
-  //           />
-  //   fetchData();
-  // }, []);
-
-  if (!browserSupportsSpeechRecognition) {
-    return <span>Browser doesn't support speech recognition.</span>;
-  }
-  return (
-    <div
-      className="d-flex justify-content-center align-items-center hero-bg"
-      style={{ backgroundImage: `url(${bgImg})` }}
-    >
-      <div className="search-container d-flex justify-content-center">
+  return(
+    <div className="d-flex justify-content-center align-items-center hero-bg" style={{backgroundImage: `url(${bgImg})`}}>
+      <div className="search-container d-flex justify-content-center flex-column p-4">
         <form className="d-flex" onSubmit={searchSubmit}>
           <div className="input-container d-flex">
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search books, ISBN, Author"
-              aria-label="Search"
-              onChange={(e) => setSearchInput(e.target.value)}
+            <input 
+            className="form-control mr-sm-2" 
+            name="search"
+            type="search" 
+            placeholder="Search books, ISBN, Author" 
+            aria-label="Search" 
+            // value={voiceSearch}
+            // onChange={onChangeHandler}
+            // onChange={(e) => setSearchInput(e.target.value)}
             />
-            <span className="btn btn-light mx-2 sp-btn">
-              <i className="fas fa-camera"></i>
-            </span>
-            <span className="btn btn-light sp-btn">
-              <button onClick={SpeechRecognition.startListening}>
-                <i className="fas fa-microphone-alt"></i>Start
-              </button>
-            </span>
           </div>
           <button className="btn btn-theme mx-2">Search</button>
+          <span className="btn btn-light sp-btn w-100 border-input"
+            onClick={SpeechRecognition.startListening}
+            ><i className="fas fa-microphone-alt"></i></span>
         </form>
         <div className="m-1">
-          <p className="m-1 fs-3">Or you can search using</p>
+          <p className="mb-0">{transcript}</p>
         </div>
         <div className="d-flex">
           {/* <ImageUpload 
             setImageLoading={setImageLoading}
           /> */}
-          <span className="btn btn-light sp-btn w-100 border-input"><i className="fas fa-microphone-alt"></i></span>
+          {/* <span className="btn btn-light sp-btn w-100 border-input"><i className="fas fa-microphone-alt"></i></span> */}
         </div>
       </div>
-      <p>Microphone: {listening ? "on" : "off"}</p>
     </div>
-  );
-}
+  )
+};
